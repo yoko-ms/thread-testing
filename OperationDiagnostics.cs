d8a1a0571c89f082e0c594942b061e0458467653
yoko-ms <yoko@microsoft.com>
2022-03-13 18:59:01 -0700

    thread testing code

---
+++ OperationDiagnostics.cs
@@ -0,0 +1,103 @@
+﻿// ----------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ----------------------------------------------------------------
+
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace TestThreading
+{
+    public class OperationDiagnostics
+    {
+        private Stopwatch stopWatcher;
+
+        public string Name { get; set; }
+        public string Message { get; set; }
+        public string ResultSignature { get; set; }
+        public long ElapsedTime { get; set; }
+        public string ClientData { get; set; }
+
+        public void StartTimer()
+        {
+            this.stopWatcher = new Stopwatch();
+            this.stopWatcher.Start();
+        }
+
+        public void StopTimer()
+        {
+            this.stopWatcher.Stop();
+            this.ElapsedTime = this.stopWatcher.ElapsedMilliseconds;
+        }
+
+        /// <summary>
+        /// Delegate for action with latency tracking
+        /// </summary>
+        /// <param name="diagnostics">Operation Diagnostics object</param>
+        public delegate void TrackLatencyAction(ref OperationDiagnostics diagnostics);
+
+        /// <summary>
+        /// Delegate for async action with latency tracking
+        /// </summary>
+        /// <returns>Modified Diagnostics</returns>
+        /// <param name="diagnostics">Operation Diagnostics object</param>
+        public delegate Task<OperationDiagnostics> TrackLatencyAsyncAction(OperationDiagnostics diagnostics);
+
+        /// <summary>
+        /// Helper for logging latency metrics and Ifx events
+        /// </summary>
+        /// <param name="latencyAction">Traced operation</param>
+        /// <param name="name">Name of operation.</param>
+        /// <returns>Traced event</returns>
+        public static OperationDiagnostics TraceRequestAction(TrackLatencyAction latencyAction, string name)
+        {
+            OperationDiagnostics operation = new OperationDiagnostics();
+            operation.Name = name;
+            operation.StartTimer();
+
+            try
+            {
+                latencyAction(ref operation);
+                operation.Message = "None";
+                operation.ResultSignature = "OK";
+            }
+            catch (Exception ex)
+            {
+                operation.Message = ex.ToString();
+                operation.ResultSignature = "Failed";
+            }
+
+            operation.StopTimer();
+            return operation;
+        }
+
+        /// <summary>
+        /// Helper for logging latency metrics and Ifx events
+        /// </summary>
+        /// <param name="latencyAction">Traced operation</param>
+        /// <param name="name">Name of operation.</param>
+        /// <returns>Traced event</returns>
+        public static async Task<OperationDiagnostics> TraceRequestAsyncAction(TrackLatencyAsyncAction latencyAction, string name)
+        {
+            OperationDiagnostics operation = new OperationDiagnostics();
+            operation.StartTimer();
+            operation.Name = name;
+
+            try
+            {
+                operation = await latencyAction(operation);
+                operation.Message = "None";
+                operation.ResultSignature = "OK";
+            }
+            catch (Exception ex)
+            {
+                operation.Message = ex.ToString();
+                operation.ResultSignature = "Failed";
+            }
+
+            operation.StopTimer();
+            return operation;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestThreading
+{
+    class Program
+    {
+        private static Random rand = new Random();
+        private const int NumberOfTasks = 100;
+        private const int WaitTimeInMs = 2000;
+
+        private static int maxConcurrentTasks;
+
+        private static ConcurrentQueue<OperationDiagnostics> resultQueue = new ConcurrentQueue<OperationDiagnostics>();
+
+        static void Main(string[] arg)
+        {
+            maxConcurrentTasks = 0;
+            TestCaseOne();
+
+            Thread.Sleep(1000);
+
+            maxConcurrentTasks = 0;
+            TestCaseTwo();
+        }
+
+        private static void updateMaxTasks()
+        {
+            int number = Process.GetCurrentProcess().Threads.Count;
+            if (number > maxConcurrentTasks)
+            {
+                Interlocked.Exchange(ref maxConcurrentTasks, number);
+            }
+        }
+
+        private static void TestCaseOne()
+        {
+            Task<OperationDiagnostics>[] tasks = new Task<OperationDiagnostics>[NumberOfTasks];
+            for (int i = 0; i < NumberOfTasks; i++)
+            {
+                tasks[i] = OperationDiagnostics.TraceRequestAsyncAction(
+                    async (diagnostic) =>
+                    {
+                        updateMaxTasks();
+                        resultQueue.Enqueue(diagnostic);
+
+                        await Task.Run(() =>
+                            {
+                                updateMaxTasks();
+                                for (int j = 0; j < 10; j++)
+                                {
+                                    diagnostic.ClientData += string.Format("counting-{0: 3:D2}", j);
+                                    Thread.Sleep(50);
+                                }
+                            });
+
+                        return diagnostic;
+                    },
+                    string.Format("{0, 3:D2}", i));
+                Thread.Sleep(100);
+            }
+
+            Task.WaitAll(tasks);
+
+            long avg = 0, maxWait = 0;
+            Console.WriteLine("Task with Task.Run()");
+            foreach (OperationDiagnostics data in resultQueue)
+            {
+                avg += data.ElapsedTime;
+                maxWait = Math.Max(maxWait, data.ElapsedTime);
+
+                Console.Write($" {data.ElapsedTime}");
+            }
+
+            Console.WriteLine($"\nMaxTasks: {maxConcurrentTasks}, Avg: {avg / NumberOfTasks} (ms), maxWait: {maxWait} (ms)");
+        }
+
+        private static void TestCaseTwo()
+        {
+            resultQueue.Clear();
+
+            Task<OperationDiagnostics>[] tasks = new Task<OperationDiagnostics>[NumberOfTasks];
+            for (int i = 0; i < NumberOfTasks; i++)
+            {
+                tasks[i] = OperationDiagnostics.TraceRequestAsyncAction(
+                    (diagnostic) =>
+                    {
+                        updateMaxTasks();
+                        resultQueue.Enqueue(diagnostic);
+
+                        for (int j = 0; j < 10; j++)
+                        {
+                            updateMaxTasks();
+                            diagnostic.ClientData += string.Format("counting-{0: 3:D2}", j);
+                            Thread.Sleep(50);
+                        }
+
+                        return Task.FromResult(diagnostic);
+                    },
+                    string.Format("{0, 3:D2}", i));
+                Thread.Sleep(100);
+            }
+
+            Task.WaitAll(tasks);
+
+            long avg = 0, maxWait = 0;
+            Console.WriteLine("Task with Task.FromResult()");
+            foreach (OperationDiagnostics data in resultQueue)
+            {
+                avg += data.ElapsedTime;
+                maxWait = Math.Max(maxWait, data.ElapsedTime);
+
+                Console.Write($" {data.ElapsedTime}");
+            }
+
+            Console.WriteLine($"\nMaxTasks: {maxConcurrentTasks}, Avg: {avg / NumberOfTasks} (ms), maxWait: {maxWait} (ms)");
+        }
+    }
+}
